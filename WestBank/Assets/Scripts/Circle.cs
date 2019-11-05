139bb684b36dc8a73fe463209430537b711afe37
defazze <defazze@gmail.com>
2019-11-05 15:48:01 +0300

    EventSystems test

---
+++ WestBank/Assets/Scripts/Circle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Circle : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Clicked!!");
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+	{
+		Debug.Log("Enter!!");
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		Debug.Log("Exit!!");
+	}
+}
