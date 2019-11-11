854c2b252c66712bdaa674232abd8b378194a11d
defazze <defazze@gmail.com>
2019-11-11 16:22:43 +0300

    Doors begin

---
+++ WestBank/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject Prefab;
+    public float Distance = 2f;
+    void Start()
+    {
+        var prefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(Prefab, World.Active);
+        var entityManager = World.Active.EntityManager;
+
+
+        for (var x = -1; x <= 1; x++)
+        {
+            var instance = entityManager.Instantiate(prefab);
+            var position = transform.TransformPoint(new float3(x*Distance, 1f, 0));
+            entityManager.SetComponentData(instance, new Translation { Value = position });
+
+            entityManager.AddComponent(instance, typeof(RotationComponent));
+        }
+    }
+}
