4946bbaf47df97aad6bc73370c25b5598a6e49b3
defazze <defazze@gmail.com>
2019-11-20 14:10:05 +0300

    Environment

--- WestBank/Assets/Doors/Scripts/GameManager.cs
+++ WestBank/Assets/Doors/Scripts/GameManager.cs
@@ -13,7 +13,8 @@ namespace Doors
         public static GameManager Instance { get; private set; }
         public GameObject DoorPrefab;
         public GameObject BulletHolePrefab;
-        public float Distance = 2f;
+        public float distanceBetweenDoors = .2f;
+        public float doorWidth = 1.2f;
         public float maxOpenAngle = 270f;
         public float doorRotationSpeed = .7f;
         public float openPeriod = 2f;
@@ -31,17 +32,17 @@ namespace Doors
 
             entityManager = World.Active.EntityManager;
 
-
-            for (var x = -1f; x <= 1f; x++)
+            var step = doorWidth + distanceBetweenDoors;
+            for (var x = -1 * step; x <= step; x += step)
             {
                 var door = entityManager.Instantiate(doorEntity);
-                var position = transform.TransformPoint(new float3(x * Distance, 1f, 0));
-                entityManager.SetComponentData(door, new Translation { Value = position });
+                var position = new float3(x, 1f, 0);
+                entityManager.SetComponentData(door, new Translation { Value = position});
                 entityManager.AddComponentData(door, new DoorComponent
                 {
                     State = DoorState.Closed,
                     OpenTime = 0,
-                    Pivot = (float3)position + new float3(.5f, 0, 0)
+                    Pivot = (float3)position + new float3(doorWidth / 2, 0, 0)
                 });
             }
         }
